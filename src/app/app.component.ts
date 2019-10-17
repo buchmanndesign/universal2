bb26c43cb02061c660b2769d6cc4b605f0e9c9e7
John Buchmann <JBuchmann@anexinet.com>
2019-10-16 20:43:33 -0400

    no more prismic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentfulService } from './services/contentful.service';
-import { PrismicService } from './services/prismic.service';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +12,11 @@ export class AppComponent implements OnInit {
   prismicResults;
 
   constructor(
-    private contentfulService: ContentfulService,
-    private prismicService: PrismicService) {}
+    private contentfulService: ContentfulService) {}
 
   ngOnInit(): void {
     this.contentfulService.getPosts().subscribe((res) => {
       this.contentfulResults = res.items;
-    });
-
-    this.prismicService.getPosts().subscribe((res) => {
-      this.prismicResults = res.results;
-      console.log(res.results)
     });
   }
 }
